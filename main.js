647650b6e64d9a2bcec7ef3554118042dabcb477
cruzguadarrama12 <cruzguadarrama12@gmail.com>
2025-03-17 01:06:37 +0000

    intento hacer que mi navbar se quede fija cuando haga scroll a la pagina pero que no se encima a mi imagen de perfil

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-// Add JS here
+// Agrega un evento de scroll para detectar cuando se encuentra en una sección
+$(window).scroll(function() {
+    console.log('Se está ejecutando el código');
+    var scrollPosition = $(window).scrollTop();
+    var sections = $('section');
+  
+    sections.each(function() {
+      var sectionId = $(this).attr('id');
+      var sectionTop = $(this).offset().top;
+      var sectionBottom = sectionTop + $(this).height();
+  
+      if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+        // Agrega la clase activa al botón correspondiente
+        $('.nav-link[data-section="' + sectionId + '"]').addClass('active');
+      } else {
+        // Elimina la clase activa del botón correspondiente
+        $('.nav-link[data-section="' + sectionId + '"]').removeClass('active');
+      }
+    });
+  });
